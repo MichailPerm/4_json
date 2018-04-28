1aae1139b251fa480a4e1b3494e08a541fa38796
Michail_Z <apxahrejl@gmail.com>
2018-04-29 00:06:09 +0500

    Released json parsing

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,28 @@
 import json
-
+import argparse
+import sys
 
 def load_data(filepath):
-    pass
-
+    try:
+        f = open(filepath, 'r')
+        data = json.load(f)
+        f.close()
+    except Exception:
+        data = False
+    finally:
+        return data
 
 def pretty_print_json(data):
-    pass
+    json.dump(data, sys.stdout, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    args = parser.parse_args()
+    data = load_data(args.filepath)
+    if not data:
+        print ("Unable to open file %s" % args.filepath)
+    else:
+        pretty_print_json(data)
+
