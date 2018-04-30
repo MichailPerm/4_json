c673f493e3ffbacd74a2597031ec8bb176621c65
Michail_Z <apxahrejl@gmail.com>
2018-04-30 11:41:44 +0500

    Refactore

--- pprint_json.py
+++ pprint_json.py
@@ -4,33 +4,32 @@ import sys
 
 
 def load_data(filepath):
-    try:
-        json_content = None
-        with open(filepath, "r") as file:
-            json_content = json.load(file)
-            return json_content
-    except json.decoder.JSONDecodeError:
-        print("File {} does not contain json data.".format(filepath))
-    except IOError:
-        print("Unable to read file {}.".format(filepath))
+    json_content = None
+    with open(filepath, "r") as file:
+        json_content = json.load(file)
+        return json_content
 
 
 def pretty_print_json(json_content):
     json.dump(json_content, sys.stdout, indent=4, ensure_ascii=False)
 
 
-def create_args_parser(args):
+def create_args_parser(argument):
     parser = argparse.ArgumentParser()
-    for arg in args:
-        parser.add_argument(arg)
-    return parser
+    parser.add_argument(argument)
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    parser = create_args_parser(["filepath"])
-    args = parser.parse_args()
-    json_content = load_data(args.filepath)
-    if not json_content:
-        sys.exit()
+    args = create_args_parser("filepath")
+    try:
+        json_content = load_data(args.filepath)
+    except json.decoder.JSONDecodeError:
+        print("File {} does not contain json data.".format(args.filepath))
+    except IOError:
+        print("Unable to read file {}.".format(args.filepath))
     else:
-        pretty_print_json(json_content)
+        if not json_content:
+            sys.exit()
+        else:
+            pretty_print_json(json_content)
