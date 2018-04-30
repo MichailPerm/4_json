b6a21246c01a5bc975f5cbb47ff84ae825202abb
Michail_Z <apxahrejl@gmail.com>
2018-04-30 09:53:34 +0500

    Added changes by notes

--- pprint_json.py
+++ pprint_json.py
@@ -2,27 +2,35 @@ import json
 import argparse
 import sys
 
+
 def load_data(filepath):
     try:
-        file = open(filepath, 'r')
-        json_content = json.load(f)
-        file.close()
+        json_content = None
+        with open(filepath, "r") as file:
+            json_content = json.load(file)
+            return json_content
+    except json.decoder.JSONDecodeError:
+        print("File {} does not contain json data.".format(filepath))
     except IOError:
-        json_content = False
-    finally:
-        return json_content
+        print("Unable to read file {}.".format(filepath))
+
 
 def pretty_print_json(json_content):
-    json.dump(json_content, sys.stdout, indent=4)
+    json.dump(json_content, sys.stdout, indent=4, ensure_ascii=False)
 
 
-if __name__ == '__main__':
+def create_args_parser(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('filepath')
+    for arg in args:
+        parser.add_argument(arg)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_args_parser(["filepath"])
     args = parser.parse_args()
     json_content = load_data(args.filepath)
     if not json_content:
-        print ("Unable to open file %s" % args.filepath)
+        sys.exit()
     else:
         pretty_print_json(json_content)
-
